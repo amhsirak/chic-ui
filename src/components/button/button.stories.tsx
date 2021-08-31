6274b21fa56f384e2d8e62f939dc59f6f145e180
karishmas6 <carishmashukla28@gmail.com>
2021-08-31 18:01:07 +0530

    button component

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -31,3 +31,67 @@ HiddenButton.args = {
   children: 'Hidden Button',
   type: 'hidden',
 };
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: 'Disabled Button',
+  disabled: true
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  children: 'Button',
+  loading: true,
+};
+
+const ButtonRow = styled.div `
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 12px;
+  & > * {
+    margin-right: 12px;
+  }
+`;
+
+export const Sizes = () => {
+  return (
+    <>
+    <ButtonRow>
+      <Button size='extraLarge'>Extra Large</Button>
+      <Button size='large'>Large</Button>
+      <Button size='default'>Default</Button>
+      <Button size='small'>Small</Button>
+    </ButtonRow>
+    <ButtonRow>
+        <Button type='secondary' size='extraLarge'>Extra Large</Button>
+        <Button type='secondary' size='large'>Large</Button>
+        <Button type='secondary' size='default'>Default</Button>
+        <Button type='secondary' size='small'>Small</Button>
+      </ButtonRow>
+      <ButtonRow>
+        <Button type='danger' size='extraLarge'>Extra Large</Button>
+        <Button type='danger' size='large'>Large</Button>
+        <Button type='danger' size='default'>Default</Button>
+        <Button type='danger' size='small'>Small</Button>
+      </ButtonRow>
+      <ButtonRow>
+        <Button type='hidden' size='extraLarge'>Extra Large</Button>
+        <Button type='hidden' size='large'>Large</Button>
+        <Button type='hidden' size='default'>Default</Button>
+        <Button type='hidden' size='small'>Small</Button>
+      </ButtonRow>
+      <ButtonRow>
+        <Button disabled size='extraLarge'>Extra Large</Button>
+        <Button disabled size='large'>Large</Button>
+        <Button disabled size='default'>Default</Button>
+        <Button disabled size='small'>Small</Button>
+      </ButtonRow>
+      <ButtonRow>
+        <Button loading size='extraLarge'>Extra Large</Button>
+        <Button loading size='large'>Large</Button>
+        <Button loading size='default'>Default</Button>
+        <Button loading size='small'>Small</Button>
+      </ButtonRow>
+      </>
+  );
+};
