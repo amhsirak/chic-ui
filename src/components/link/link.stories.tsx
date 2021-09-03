02f493c8f677c1728f8c4c3cb283833cd1b91c9c
karishmas6 <carishmashukla28@gmail.com>
2021-09-04 02:20:41 +0530

    Link Componeny

---
+++ src/components/link/link.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import Link, { LinkProps } from './link';
+
+export default {
+  title: 'Components/Link',
+  component: Link,
+} as Meta;
+
+const Template: Story<LinkProps> = (args) => <Link {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Link',
+  href: '#',
+};
+
+export const CustomElement = Template.bind({});
+CustomElement.args = {
+  children: 'I am span',
+  href: '#',
+  as: 'span',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: 'Disabled link',
+  href: '#',
+  disabled: true,
+};
