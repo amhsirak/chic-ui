7299c04c22d4663c07852af00b9afc1d6874a83e
karishmas6 <carishmashukla28@gmail.com>
2021-09-04 23:01:34 +0530

    Skeleton component

---
+++ src/components/skeleton/skeleton.tsx
@@ -0,0 +1,1 @@
+import { StyledSkeleton } from './styled';
---
+++ src/components/skeleton/styled.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+interface StyledSkeletonProps {
+    width: number;
+    height: number;
+    borderRadius: string;
+}
+
+export const StyledSkeleton =  styled.div<StyledSkeletonProps>`
+    position: relative;
+    overflow: hidden;
+    width: ${pr => pr.width}px;
+    height: ${pr => pr.height}px;
+    border-radius: ${pr => pr.borderRadius};
+    background-color: #e3e3e3;
+
+    &:before {
+        content: '';
+        display: block;
+        position: absolute;
+        left: -${pr => pr.width}px;
+        top: 0;
+        height: ${pr => pr.height}px;
+        width: ${pr => pr.width}px;
+        background-color: #cccccc;
+        animation: load 1.5s cubic-bezier(0.4, 0.0, 0.2, 1);
+        animation-iteration-count: infinite;
+    }
+    
+    @keyframes load {
+        from {
+            left: -${pr => pr.width}px;
+        }
+        to {
+            left: 100%;
+        }
+    }
+`;
