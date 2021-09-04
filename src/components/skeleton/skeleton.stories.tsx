0730b60fe4227705362b9ea496b7b60556f57b52
karishmas6 <carishmashukla28@gmail.com>
2021-09-04 23:33:43 +0530

    Skeleton Component

---
+++ src/components/skeleton/skeleton.stories.tsx
@@ -0,0 +1,22 @@
+import { Story, Meta } from '@storybook/react';
+import Skeleton, { SkeletonProps } from './skeleton';
+
+export default {
+    title: 'Components/Skeleton',
+    component: Skeleton,
+} as Meta;
+
+const Template: Story<SkeletonProps> = (args) => <Skeleton {...args} />
+
+export const Default = Template.bind({});
+Default.args = {
+    width: 150,
+    height: 150,
+}
+
+export const BorderRadius = Template.bind({})
+BorderRadius.args = {
+    width: 150,
+    height: 150,
+    borderRadius: '50%',
+}
