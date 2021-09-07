4d557e2e07c6fdff7486d578ea5e53ff1b92c79d
karishmas6 <carishmashukla28@gmail.com>
2021-09-07 22:19:41 +0530

    text input

--- src/components/text-input/styled.ts
+++ src/components/text-input/styled.ts
@@ -22,3 +22,51 @@ interface StyledTextInputProps {
     withCross: boolean;
 }
 
+export const StyledTextInpt = styled.input<StyledTextInputProps>`
+    box-sizing: border-box;
+    position: relative;
+    color: #000;
+    border: none;
+    border-radius: 0;
+    outline: none;
+    transition: 0.1s ease-out;
+    padding: 0;
+    padding-left: ${pr => sidePaddings[pr.innerSize] + (pr.withIcon ? sidePaddings[pr.innerSize] + 10/* icon */ : 0)}px;
+    padding-right: ${pr => sidePaddings[pr.innerSize] + (pr.withIcon ? sidePaddings[pr.innerSize] : 0)}px;
+    height: ${pr => heights[pr.innerSize]}px;
+    width: ${pr => pr.width};
+    box-shadow: inset 0 0 0 2px ${pr => pr.error ? '#d93848' : 'transparent'};
+    background-color: ${pr => pr.error ? 'ffe3e6' : '#EEEEEE'};
+    &:focus {
+        box-shadow: inset 0 0 0 2px ${pr => pr.error ? '#d93848' : '#000'};
+    }
+
+    // Disabled
+    ${pr => pr.disabled ? `
+        background-color: #a6a6a6;
+        color: #5e5e5e;
+        cursor: not-allowed;
+        
+        &:hover {
+            background-color: #a6a6a6 !important;
+            color: #5e5e5e !important;
+        }
+    `: ''}
+`;
+
+interface StyledIconProps {
+    innerSize: ComponentSize;
+}
+
+export const StyledIcon = styled.div.withConfig({
+    shouldForwardProp: (prop, defPropValFN) => 
+    !['innerSize'].includes(prop) && defPropValFN(prop),
+})<StyledIconProps>`
+    box-sizing: border-box;
+    postion: absolute;
+    left: ${ pr => sidePaddings[pr.innerSize]}px;
+    top: 50%;
+    transform: translateY(-50%);
+    height: 15px;
+    width: 15px;
+`;
