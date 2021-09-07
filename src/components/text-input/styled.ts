56357428e45dc62d9620517bb6dc3c7cda5f0ba3
karishmas6 <carishmashukla28@gmail.com>
2021-09-07 17:46:59 +0530

    text input

--- src/components/text-input/styled.ts
+++ src/components/text-input/styled.ts
@@ -1,2 +1,24 @@
 import styled from 'styled-components';
 import { ComponentSize, heights, sidePaddings } from '../../config/sizes';
+import CrossIcon from '../../icons/Cross';
+
+interface StyledWrapperProps {
+    width: string;
+    innerSize: ComponentSize;
+}
+
+export const StyledWrapper = styled.div<StyledWrapperProps>`
+    position: relative;
+    width: ${pr => pr.width};
+    height: ${pr => heights[pr.innerSize]}px;
+`;
+
+interface StyledTextInputProps {
+    innerSize: ComponentSize;
+    disabled: boolean;
+    error: boolean;
+    width: string;
+    withIcon: boolean;
+    withCross: boolean;
+}
+
