1fdfc0d688c35922d1e8417faca0e40f701123c5
karishmas6 <carishmashukla28@gmail.com>
2021-09-09 18:32:48 +0530

    exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,9 @@
 export { default as Button } from './button';
+
+export { default as Link } from './link';
+
+export { default as Skeleton } from './skeleton';
+
+export { default as Spinner } from './spinner';
+
+export { default as TextInput } from './text-input';
