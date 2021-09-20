45944163469bb7406f4fa9340888d1a7dfe7a91f
karishmas6 <carishmashukla28@gmail.com>
2021-09-20 22:51:16 +0530

    Search component

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,8 +1,9 @@
+import React, { ChangeEventHandler } from 'react';
 import Button from '../button';
-import TextInput  from '../text-input';
-import { SearchWrapper } from './styled';
+import TextInput from '../text-input';
 import { ComponentSize } from '../../config/sizes';
-import { ChangeEventHandler } from 'react';
+import { SearchWrapper } from './styled';
+import SearchIcon from '../../icons/Search';
 
 export interface SearchProps {
     className?: string;
@@ -12,3 +13,39 @@ export interface SearchProps {
     onChange?: ChangeEventHandler<HTMLInputElement>;
     placeholder?: string;
 }
+
+const Search: React.ForwardRefRenderFunction<HTMLDivElement, SearchProps> = (props, ref) => {
+    const { 
+        size = 'default',
+        className,
+        value,
+        onChange,
+        placeholder,
+        width = '100%',
+    } = props;
+
+    const textInputStyles = {
+        size,
+        width: '100%',
+        placeholder
+    }
+
+    return (
+        <SearchWrapper
+            ref={ref}
+            className={className}
+            width={width}
+        >
+            <TextInput
+                value={value}
+                onChange={onChange}
+                //placeholder={placeholder}
+                clearable
+                {...textInputStyles}
+            />
+            <Button type='secondary' icon={SearchIcon} />
+        </SearchWrapper>
+    );
+}
+
+export default React.forwardRef<HTMLDivElement, SearchProps>(Search);
