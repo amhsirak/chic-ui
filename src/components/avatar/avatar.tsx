678a7ce53b2b59be360cc61d1e8c0638b0b6216a
karishmas6 <carishmashukla28@gmail.com>
2021-09-26 22:33:14 +0530

    Avatar component

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+interface AvatarProps {
+    src: string;
+    className?: string;
+    width?: string;
+    height?: string;
+    name?: string;
+    hasBadge?: boolean;
+    isActive?: boolean;
+}
--- src/components/avatar/styled.ts
+++ src/components/avatar/styled.ts
@@ -1,1 +1,20 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+
+interface StyledAvatarProps {
+    src: string;
+    className?: string;
+    width?: string;
+    height?: string;
+    name?: string;
+    hasBadge?: boolean;
+    isActive?: boolean;
+}
+
+const StyledAvatar = styled.span<StyledAvatarProps>`
+    display: block;
+    position: relative;
+    width: ${pr => pr.width}px;
+    height: ${pr => pr.height}px;
+    padding: 5px;
+    border-radius: 50%;
+`;
