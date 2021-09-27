ec6f537691150a58c5b02afa71a3e9e88f6ec4f4
karishmas6 <carishmashukla28@gmail.com>
2021-09-27 18:32:54 +0530

    Badge component

--- src/components/badge/badge.stories.tsx
+++ src/components/badge/badge.stories.tsx
@@ -23,15 +23,9 @@ export const Sizes = () => {
   return (
     <>
     <BadgeRow>
-      <Badge size='extraLarge'>Extra Large</Badge>
-      <Badge size='large'>Large</Badge>
-      <Badge size='default'>Default</Badge>
-      <Badge size='small'>Small</Badge>
-    </BadgeRow>
-    <BadgeRow>
       <Badge>Default</Badge>
       <Badge type='secondary'>Secondary</Badge>
-      <Badge type='danger'>Default</Badge>
+      <Badge type='danger'>Danger</Badge>
       <Badge type='success'>Success</Badge>
       <Badge type='warning'>Warning</Badge>
       <Badge type='light'>Light</Badge>
@@ -39,7 +33,7 @@ export const Sizes = () => {
     <BadgeRow>
       <Badge rounded>Default</Badge>
       <Badge type='secondary' rounded>Secondary</Badge>
-      <Badge type='danger' rounded>Default</Badge>
+      <Badge type='danger' rounded>Danger</Badge>
       <Badge type='success' rounded>Success</Badge>
       <Badge type='warning' rounded>Warning</Badge>
       <Badge type='light' rounded>Light</Badge>
--- src/config/sizes.ts
+++ src/config/sizes.ts
@@ -17,21 +17,3 @@ export const heights: {
     large: 55,
     extraLarge: 65
 }
-
-export const badgeSidePaddings: {
-    [key in ComponentSize]: number
-} = {
-    default: 15,
-    small: 13,
-    large: 17,
-    extraLarge: 19
-}
-
-export const badgeHeights: {
-    [key in ComponentSize]: number
-} = {
-    default: 25,
-    small: 20,
-    large: 30,
-    extraLarge: 35
-}
