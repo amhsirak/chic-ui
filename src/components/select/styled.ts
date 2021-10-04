350622bad0372cf0601d03230e33bb2857385550
mohan-murali <mohanmuralid28@gmail.com>
2021-10-04 09:15:45 +0530

    Updated the styles of select component

--- src/components/select/styled.ts
+++ src/components/select/styled.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { ComponentSize, heights } from '../../config/sizes';
+import { ComponentSize, heights, sidePaddings } from '../../config/sizes';
+import SvgArrowDown from '../../icons/ArrowDown';
 
 interface SelectWrapperProps {
   width: string;
@@ -14,7 +15,6 @@ export const SelectWrapper = styled.div<SelectWrapperProps>`
 export const SelectOption = styled.li`
   list-style: none;
   border-radius: 2px;
-  border: 1px solid black;
   padding: 8px;
   &:hover {
     background-color: rgba(144, 202, 249, 0.6);
@@ -28,15 +28,33 @@ interface StyledSelectDivProps {
 }
 
 export const StyledSelectDiv = styled.div<StyledSelectDivProps>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   width: 100%;
   padding: 8px;
-  border-radius: 2px;
+  border-radius: 8px;
+  transition: 0.1s ease-out;
   box-shadow: inset 0 0 0 2px ${(pr) => (pr.error ? '#d93848' : 'transparent')};
   background-color: ${(pr) => (pr.error ? 'ffe3e6' : '#EEEEEE')};
   height: ${(pr) => heights[pr.innerSize]}px;
   &:focus {
     box-shadow: inset 0 0 0 2px ${(pr) => (pr.error ? '#d93848' : '#000')};
   }
+
+  //Disabled
+  ${(pr) =>
+    pr.disabled &&
+    `
+        background-color: #a6a6a6;
+        color: #5e5e5e;
+        cursor: not-allowed;
+
+        &:hover {
+            background-color: #a6a6a6 !important;
+            color: #5e5e5e !important;
+        }
+    `}
 `;
 
 export const SelectOptionWrapper = styled.div`
@@ -45,4 +63,9 @@ export const SelectOptionWrapper = styled.div`
   &:hover {
     cursor: pointer;
   }
+`;
+
+export const ArrowDown = styled(SvgArrowDown)`
+  box-sizing: border-box;
+  height: 15px;
 `;
