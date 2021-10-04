3854675729b9714ca39a91878c6d72937ccaedcb
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-10-04 23:14:40 +0200

    added width and height main parameters to card

--- src/components/card/styled.ts
+++ src/components/card/styled.ts
@@ -9,6 +9,8 @@ export const StyledCard = styled.div<StyledCardProps>`
   margin: 0;
   padding: 0;
   font-size: 15px;
+  height: ${(pr) => pr.height};
+  width: ${(pr) => pr.width};
 `;
 
 export const StyledImage = styled.img`
