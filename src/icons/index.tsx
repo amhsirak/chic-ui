08c4ec4c790273011daae581edaa18e08d4685f2
karishmas6 <carishmashukla28@gmail.com>
2021-11-17 05:09:28 +0530

    updates

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,4 +1,3 @@
 export { default as ArrowDown } from './ArrowDown'
 export { default as Cross } from './Cross'
-export { default as Download } from './Download'
 export { default as Search } from './Search'
