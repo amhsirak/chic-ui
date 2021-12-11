781db583c0179e9f7994f3b72e36711e70b76ce4
karishmas6 <carishmashukla28@gmail.com>
2021-12-11 18:49:00 +0530

    update imports

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEventHandler } from 'react';
-import Button from '../button';
-import TextInput from '../text-input';
-import { ComponentSize } from '../../config/sizes';
+import Button from 'components/button';
+import TextInput from 'components/text-input';
+import { ComponentSize } from 'config/sizes';
 import { SearchWrapper } from './styled';
 
 export interface SearchProps {
