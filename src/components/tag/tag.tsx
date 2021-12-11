7f12a8ee5580ba9f2706d10f4612960e3ed30b13
karishmas6 <carishmashukla28@gmail.com>
2021-12-11 18:55:32 +0530

    update imports

--- src/components/tag/tag.tsx
+++ src/components/tag/tag.tsx
@@ -1,6 +1,6 @@
 import React, { ElementType, ReactNode } from 'react';
 import { StyledTag } from './styled';
-import { themeType } from '../../config/themes';
+import { themeType } from 'config/themes';
 
 export interface BaseTagProps {
   type?: themeType;
