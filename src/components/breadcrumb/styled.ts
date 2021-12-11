2b1e78d20c8f85127a64e9af7f161368f923b2db
karishmas6 <carishmashukla28@gmail.com>
2021-12-11 18:33:46 +0530

    update breadcumb component

--- src/components/breadcrumb/styled.ts
+++ src/components/breadcrumb/styled.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
-interface StyledBreadcrumbProps {}
-
-export const StyledBreadcrumb = styled.ol<StyledBreadcrumbProps>`
+export const StyledBreadcrumb = styled.ol`
   list-style: none;
   display: flex;
   align-items: center;
