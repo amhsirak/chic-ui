6813df5a6482f2ed8ed52edaa271736824cd59a5
karishmas6 <carishmashukla28@gmail.com>
2021-12-12 19:58:02 +0530

    update global styles

--- src/config/global.styles.ts
+++ src/config/global.styles.ts
@@ -1,16 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-    html,body {
+    * {
+        box-sizing: border-box;
         margin: 0;
         padding: 0;
         font-family: 'Arial';
     }
-    * {
-        box-sizing: border-box; 
-    }
     a {
-        color: inherit;
         text-decoration: none;
     }
 `;
