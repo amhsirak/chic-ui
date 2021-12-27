506e15667f6716e72ea595b4b07852e4705c201a
karishmas6 <carishmashukla28@gmail.com>
2021-12-28 02:48:34 +0530

    add styles

---
+++ src/components/cardgroup/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { theme as typeColors } from 'config/themes';
+
+export const StyledCards = styled.div`
+  display: flex;
+  -webkit-display: box;
+  -moz-display: box;
+  -ms-display: flexbox;
+  -webkit-display: flex;
+  flex-wrap: wrap;
+  padding: 0;
+  margin: 0;
+`;
