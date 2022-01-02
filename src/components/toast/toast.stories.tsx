74d137e77d0b34581701fa59979ed751dcd65c2b
karishmas6 <carishmashukla28@gmail.com>
2022-01-02 21:11:18 +0530

    update toast component

--- src/components/toast/toast.stories.tsx
+++ src/components/toast/toast.stories.tsx
@@ -1,19 +1,29 @@
-import { Meta } from '@storybook/react';
-import Toast  from '.';
+import { Meta } from '@storybook/react/types-6-0';
+import Toast from './toast';
+import { toast } from './toastmanager';
+import SearchIcon from '../../icons/Search';
 
 export default {
   title: 'Components/Toast',
   component: Toast
 } as Meta;
 
-export const All = () => {
+// display a toast message when the page loads
+// you can call toast.destroy('my-id') from anywhere to remove this message manually with code
 
+export const Trying = () => {
   return (
-    <>
-      <Toast type="danger" title="LOGIN FAILED" message="Please enter correct credentials.Please enter correct credentialsPlease enter correct credentialsPlease enter correct credentialsPlease enter correct credentials" />
-      <Toast type="info" title="Info Toast" message="Some information" />
-      <Toast type="success" title="Success Toast" message="Success yaayyy" />
-      <Toast type="warning" title="Warning Toast" message="Warning warning warning" />
-    </>
+    <button
+      onClick={() =>
+        toast.open({
+          title: 'Toast title',
+          message: 'Toast body',
+          type: 'secondary',
+          duration: 3000
+        })
+      }
+    >
+      Show toast
+    </button>
   );
 };
