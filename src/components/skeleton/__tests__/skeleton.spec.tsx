36f742c582a1407f2bcd9a7ccb7df6b3b7b56e18
karishmas6 <carishmashukla28@gmail.com>
2022-01-04 00:07:39 +0530

    add tests

---
+++ src/components/skeleton/__tests__/skeleton.spec.tsx
@@ -0,0 +1,15 @@
+import Skeleton from '..';
+import { render } from 'test/utils';
+
+describe('Skeleton Component', () => {
+  it('Renders Correctly', () => {
+    const SkeletonGroup = render(
+      <>
+        <Skeleton />
+        <Skeleton width={80} height={60} />
+        <Skeleton borderRadius="50%" />
+      </>
+    );
+    expect(SkeletonGroup).toMatchSnapshot();
+  });
+});
