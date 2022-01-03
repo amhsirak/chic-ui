eb66bd06643704fd11d10ee51a950a425b5b07cb
karishmas6 <carishmashukla28@gmail.com>
2022-01-04 05:18:32 +0530

    fix bugs + deploy v1.0.2

--- lib/components/select/styled.d.ts
+++ lib/components/select/styled.d.ts
@@ -15,7 +15,7 @@ interface StyledSelectDivProps {
     disabled: boolean;
 }
 export declare const StyledSelectDiv: import("styled-components").StyledComponent<"div", any, StyledSelectDivProps, never>;
-export declare const SelectOptionWrapper: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const SelectOptionWrapper: import("styled-components").StyledComponent<"ul", any, {}, never>;
 export declare const ArrowDown: import("styled-components").StyledComponent<typeof SvgArrowDown, any, {}, never>;
 export declare const StyledCross: import("styled-components").StyledComponent<typeof Cross, any, {}, never>;
 export declare const MultiSelectOptionWrapper: import("styled-components").StyledComponent<"span", any, {}, never>;
