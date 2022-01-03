b8226249468af0e5babda659efa92cefe978189c
karishmas6 <carishmashukla28@gmail.com>
2022-01-04 00:09:46 +0530

    add test utils

---
+++ src/test/utils.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+import renderer from 'react-test-renderer';
+
+import ThemeProvider from 'components/themeprovider'
+
+// Provides UI Context to tested components
+export const render = (node: ReactNode ) =>
+  renderer.create(
+  <ThemeProvider>
+      {node}
+  </ThemeProvider>).toJSON()
