59a860d281f1d705b004113f02de42a3b0003dfd
karishmas6 <carishmashukla28@gmail.com>
2022-01-04 00:07:27 +0530

    add tests

---
+++ src/components/select/__tests__/select.spec.tsx
@@ -0,0 +1,35 @@
+import Select from '..';
+import { render } from 'test/utils';
+
+describe('Select Component', () => {
+  it('Renders Correctly', () => {
+    const SelectGroup = render(
+      <>
+        <div>
+          <Select
+            width="200px"
+            data={['India', 'Japan', 'Germany', 'Russia']}
+            placeholder="Select a country"
+            onChange={(value) => alert(value)}
+          />
+        </div>
+        <div>
+          <Select
+            multiSelect
+            width="400px"
+            data={[
+              { id: 1, game: 'Hockey' },
+              { id: 2, game: 'Football' },
+              { id: 3, game: 'Tennis' }
+            ]}
+            dataKey="id"
+            dataName="game"
+            placeholder="Select a sport"
+            onChange={(value) => alert(value)}
+          />
+        </div>
+      </>
+    );
+    expect(SelectGroup).toMatchSnapshot();
+  });
+});
