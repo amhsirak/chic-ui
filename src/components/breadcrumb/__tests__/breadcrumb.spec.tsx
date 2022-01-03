22be40a363349984d0083e9e8e459baf1c8ddeba
karishmas6 <carishmashukla28@gmail.com>
2022-01-04 00:00:13 +0530

    add tests

---
+++ src/components/breadcrumb/__tests__/breadcrumb.spec.tsx
@@ -0,0 +1,23 @@
+import Breadcrumb from '..';
+import { render } from 'test/utils';
+
+describe('Breadcrumb Component', () => {
+  it('Renders Correctly', () => {
+    const BreadcrumbGroup = render(
+      <>
+        <Breadcrumb separator="/">
+          <div>
+            <a href="https://www.google.com">Google</a>
+          </div>
+          <div>
+            <a href="https://facebook.com">Facebook</a>
+          </div>
+          <div>
+            <a href="https://twitter.com">Twitter</a>
+          </div>
+        </Breadcrumb>
+      </>
+    );
+    expect(BreadcrumbGroup).toMatchSnapshot();
+  });
+});
