210d2f5e3e5fcca4a109bdd326c3af74276e7f23
karishmas6 <carishmashukla28@gmail.com>
2022-01-04 00:01:46 +0530

    add tests

---
+++ src/components/image/__tests__/image.spec.tsx
@@ -0,0 +1,17 @@
+import Image from '..';
+import { render } from 'test/utils';
+
+describe('Image Component', () => {
+  it('Renders Correctly', () => {
+    const ImageGroup = render(
+      <>
+        <Image src="cat1.png" width="160px" height="130px" />
+        <Image src="cat2.png" width="160px" height="130px" />
+        <Image src="cat1.png" width="160px" height="130px" rounded />
+        <Image src="cat2.png" width="160px" height="130px" circle />
+        <Image src="cat3.png" width="160px" height="130px" outline />
+      </>
+    );
+    expect(ImageGroup).toMatchSnapshot();
+  });
+});
