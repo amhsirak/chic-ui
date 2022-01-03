dae4e5ec03ecfcb6177d452867c896c43ffa3dfb
karishmas6 <carishmashukla28@gmail.com>
2022-01-04 00:06:40 +0530

    add tests

---
+++ src/components/pagination/__tests__/pagination.spec.tsx
@@ -0,0 +1,53 @@
+import Pagination from '..';
+import { render } from 'test/utils';
+
+describe('Pagination Component', () => {
+  it('Renders Correctly', () => {
+    const PaginationGroup = render(
+      <>
+        <div>
+          <Pagination
+            bgType="success"
+            pages={7}
+            startCountInZero
+            activeBg="white"
+            activeColor="black"
+            activeHoverColor="black"
+            whenNextPage={(page) =>
+              alert(`whenNextPage page hit, currently on page ${page}`)
+            }
+            whenPreviousPage={(page) =>
+              alert(`whenPreviousPage page hit, currently on page ${page}`)
+            }
+            whenPageChange={(page) =>
+              alert(`whenPageChange hit, currently on page ${page}`)
+            }
+          />
+        </div>
+        <div>
+          <Pagination
+            customBg={{
+              bgColor: 'gray',
+              hover: 'black',
+              color: 'yellow'
+            }}
+            pages={4}
+            activeBg="white"
+            activeColor="black"
+            activeHoverColor="black"
+            whenNextPage={(page) =>
+              alert(`whenNextPage page hit, currently on page ${page}`)
+            }
+            whenPreviousPage={(page) =>
+              alert(`whenPreviousPage page hit, currently on page ${page}`)
+            }
+            whenPageChange={(page) =>
+              alert(`whenPageChange hit, currently on page ${page}`)
+            }
+          />
+        </div>
+      </>
+    );
+    expect(PaginationGroup).toMatchSnapshot();
+  });
+});
