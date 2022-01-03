e4420b181a80237e3400e905113fa13616a7bb84
karishmas6 <carishmashukla28@gmail.com>
2022-01-04 00:07:51 +0530

    add tests

---
+++ src/components/spinner/__tests__/spinner.spec.tsx
@@ -0,0 +1,19 @@
+import Spinner from '..';
+import { render } from 'test/utils';
+
+describe('Spinner Component', () => {
+  it('Renders Correctly', () => {
+    const SpinnerGroup = render(
+      <>
+        <Spinner size={40} />
+        <Spinner type="primary" />
+        <Spinner type="secondary" />
+        <Spinner type="danger" />
+        <Spinner type="success" />
+        <Spinner type="warning" />
+        <Spinner type="info" />
+      </>
+    );
+    expect(SpinnerGroup).toMatchSnapshot();
+  });
+});
