ad4017d29efa7384cbf69e7f2347424a606296f8
karishmas6 <carishmashukla28@gmail.com>
2022-01-04 00:08:03 +0530

    add tests

---
+++ src/components/switch/__tests__/switch.spec.tsx
@@ -0,0 +1,27 @@
+import Switch from '..';
+import { render } from 'test/utils';
+
+describe('Switch Component', () => {
+  it('Renders Correctly', () => {
+    const SwitchGroup = render(
+      <>
+        <Switch />
+        <Switch checked />
+        <Switch checked type="success" />
+        <Switch checked type="secondary" />
+        <Switch checked type="warning" />
+        <Switch checked type="danger" />
+        <Switch checked type="info" />
+        <Switch checked type="light" />
+        <Switch checked disabled />
+        <Switch checked size="small" />
+        <Switch checked size="default" />
+        <Switch checked size="large" />
+        <Switch checked padding="small" />
+        <Switch checked padding="default" />
+        <Switch checked padding="large" />
+      </>
+    );
+    expect(SwitchGroup).toMatchSnapshot();
+  });
+});
