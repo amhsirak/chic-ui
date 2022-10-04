210fe72cec5d1866a998cea3bcf58d711c0c36ae
Gaurav Suthar <gaurav.suthar161@gmail.com>
2022-10-04 20:17:47 +0530

    create alert component

---
+++ src/components/circularProgressBar/circularProgressBar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { themeType } from '../../tokens/themes';
+import {
+  StyledCircularProgressInnerCircle,
+  StyledCircularProgressWrapper,
+  StyledLoadingText
+} from './styled';
+
+export interface CircularProgressBarProps {
+  progress?: number;
+  type?: themeType;
+  showProgress?: boolean;
+}
+
+const CircularProgressBar: React.ForwardRefRenderFunction<
+  HTMLAnchorElement,
+  CircularProgressBarProps
+> = (props, ref) => {
+  let { progress = 70, type = 'primary', showProgress = true } = props;
+
+  const degree = 3.6 * progress;
+
+  const getProgressAndDegree = (value: number, maxValue: number) => {
+    if (value < 0) return 0;
+    if (value > maxValue) return maxValue;
+
+    return value;
+  };
+
+  return (
+    <StyledCircularProgressWrapper type={type} degree={getProgressAndDegree(degree, 360)}>
+      <StyledCircularProgressInnerCircle>
+        <StyledLoadingText>
+          {showProgress ? getProgressAndDegree(progress, 100) + '%' : null}
+        </StyledLoadingText>
+      </StyledCircularProgressInnerCircle>
+    </StyledCircularProgressWrapper>
+  );
+};
+
+export default React.forwardRef<HTMLAnchorElement, CircularProgressBarProps>(
+  CircularProgressBar
+);
