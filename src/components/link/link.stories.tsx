8072d37f02c7d7e93c5a52f77dcd6134b6ab64e8
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-11-05 22:24:55 +0000

    adjusted stories to reflect type selection

--- src/components/link/link.stories.tsx
+++ src/components/link/link.stories.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
 import Link, { LinkProps } from '.';
+import { theme } from '../../tokens/themes';
 
 export default {
   title: 'Components/Link',
-  component: Link
+  component: Link,
+  argTypes: {
+    type: {
+      options: Object.keys(theme),
+      control: { type: 'select' }
+    }
+  }
 } as Meta;
 
 const Template: Story<LinkProps> = (args) => <Link {...args} />;
@@ -20,7 +27,8 @@ export const CustomElement = Template.bind({});
 CustomElement.args = {
   children: 'I am span',
   href: '#',
-  as: 'span'
+  as: 'span',
+  type: 'primary'
 };
 
 export const Disabled = Template.bind({});
