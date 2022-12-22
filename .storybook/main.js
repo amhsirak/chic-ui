6ec51d7db3eded763a3816af841b39fc858d7111
karishmas6 <carishmashukla28@gmail.com>
2022-12-22 05:56:00 +0530

    feat: set core builder and docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,10 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+  core: {
+    "builder": "webpack5"
+  },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
+  docs: {
+    docsPage: "automatic"
+  }
+};
