aa1ba82a8caa68d2c1095070af2b6ebc55cc33d3
karishmas6 <carishmashukla28@gmail.com>
2022-12-22 07:21:36 +0530

    feat: add @storybook/addon-docs as addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-docs", "@storybook/addon-essentials"],
   core: {
     builder: "@storybook/builder-webpack5"
   },
