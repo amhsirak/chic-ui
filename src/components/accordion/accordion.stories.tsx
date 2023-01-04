3306ab130ea0b5b5c8c0b794607c11109c232f5b
karishmas6 <carishmashukla28@gmail.com>
2023-01-05 00:07:33 +0530

    chore: remove unused import

--- src/components/accordion/accordion.stories.tsx
+++ src/components/accordion/accordion.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentStory, Story } from '@storybook/react';
+import { Story } from '@storybook/react';
 import React from 'react'
 import Accordion, { AccordionProps } from "./accordion";
 
