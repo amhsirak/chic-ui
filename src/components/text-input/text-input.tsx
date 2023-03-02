84acc5bd6d13ebaa8fb044b7d8b98870951aa257
karishmas6 <carishmashukla28@gmail.com>
2023-03-02 22:29:25 +0530

    feat: style prop

--- src/components/text-input/text-input.tsx
+++ src/components/text-input/text-input.tsx
@@ -22,6 +22,7 @@ export interface TextInputProps
   // if input is controlled by another element
   readonly?: boolean;
   clearable?: boolean;
+  style?: React.CSSProperties;
   [key: string]: any;
 }
 
