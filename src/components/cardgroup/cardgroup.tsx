989a8626214901ebd98d66543793133201a47235
karishmas6 <carishmashukla28@gmail.com>
2023-03-03 21:48:08 +0530

    feat: className prop

--- src/components/cardgroup/cardgroup.tsx
+++ src/components/cardgroup/cardgroup.tsx
@@ -4,16 +4,21 @@ import { StyledCards } from './styled';
 export interface StyledCardGroupProps {
   children?: ReactNode;
   style?: React.CSSProperties;
+  className?: string;
 }
 
 export const CardGroup: React.ForwardRefRenderFunction<
   HTMLDivElement,
   StyledCardGroupProps
 > = (props, ref) => {
-  const { children, style } = props;
+  const { children, style, className } = props;
 
   return (
-    <StyledCards style={style} ref={ref as React.MutableRefObject<HTMLDivElement>}>
+    <StyledCards
+      style={style}
+      className={className}
+      ref={ref as React.MutableRefObject<HTMLDivElement>}
+    >
       {children}
     </StyledCards>
   );
