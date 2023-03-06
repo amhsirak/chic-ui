cce40f519b7700ce753b0ccd677379bfc1c30e90
karishmas6 <carishmashukla28@gmail.com>
2023-03-06 17:59:47 +0530

    feat: set opacity via background

--- src/components/backdrop/styled.ts
+++ src/components/backdrop/styled.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { BackdropProps } from './backdrop';
-import { theme as typeColors } from '../../tokens/themes';
 
 export const StyledBackdrop = styled.div<BackdropProps>`
   ${(props) =>
@@ -18,6 +17,6 @@ export const StyledBackdrop = styled.div<BackdropProps>`
   width: 100vw;
   height: 100vh;
   z-index: ${(props) => (props.zIndex ? props.zIndex : 101)};
-  background: ${(props) =>
-    props.color ? props.color : typeColors['light'].hover};
+  background-color: ${(props) =>
+    props.color ? props.color : 'rgba(0,0,0,.5)'};
 `;
