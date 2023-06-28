768eb5996cb4d1b13e300407bbab5f214468c305
karishmas6 <carishmashukla28@gmail.com>
2023-06-28 14:56:32 +0530

    feat: register @storybook/react-webpack5 framework

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-docs", "@storybook/addon-essentials"],
-  core: {
-    builder: "@storybook/builder-webpack5"
-  },
   docs: {
-    docsPage: "automatic"
+    autodocs: true
+  },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   }
 };
