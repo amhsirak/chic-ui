7249be634f37dc4bc03b1d2c7801956b8a20a72e
karishmas6 <carishmashukla28@gmail.com>
2023-06-29 00:32:04 +0530

    fix: import Meta from @storybook/react

--- src/components/switch/switch.stories.tsx
+++ src/components/switch/switch.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta } from '@storybook/react/types-6-0';
+import { Meta } from '@storybook/react';
 import Switch from './switch';
 import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
