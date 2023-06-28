bb173b0cc63122aa152ed78ed6f5150ccf56b91a
karishmas6 <carishmashukla28@gmail.com>
2023-06-29 00:32:20 +0530

    fix: import Meta from @storybook/react

--- src/components/tag/tag.stories.tsx
+++ src/components/tag/tag.stories.tsx
@@ -1,4 +1,4 @@
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from '@storybook/react';
 import { theme } from '../../tokens/themes';
 import Tag, { TagProps } from '.';
 import Button from '../button';
